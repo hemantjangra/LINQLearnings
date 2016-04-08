a67a68c61e320f02e045a710b7cd8a13d88568e7
hemantjangra <hemantjangra@outlook.com>
2016-04-08 12:31:03 +0000

    First Commit Demystifying LINQ

---
+++ LINQSelect/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQSelect
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            var sequence = GenerateNumbers();
+
+            //understanding how function call happen in LINQ
+            var someSequence = GenerateNumbers();
+            someSequence = someSequence.Select(n => n * n);
+
+            //equivalent to above statement
+            someSequence = someSequence
+                                        .Select((int n) =>
+                                            {
+                                                int b = 2;
+                                                return n * b;
+                                            });
+
+            sequence = sequence.Where(s => s.ToString().Length < 2);
+            foreach (int val in someSequence)
+                Console.WriteLine(val);
+
+            Console.ReadLine();
+        }
+
+        public static IEnumerable<int> GenerateNumbers()
+        {
+            var i = 0;
+            while (i++ < 100)
+                yield return i;
+        }
+
+    }
+
+    public static class MyLinqImplementation
+    {
+        public static IEnumerable<T> Where<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            foreach (T item in source)
+                if (predicate(item))
+                    yield return item;
+        }
+    }
+}
