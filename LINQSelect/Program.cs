d79c6d9cfa68f1e838ddcde235ab153e7adaa1ab
hemantjangra <hemantjangra@outlook.com>
2016-04-13 11:36:54 +0000

    Checked in For Select Filter

--- LINQSelect/Program.cs
+++ LINQSelect/Program.cs
@@ -16,20 +16,32 @@ namespace LINQSelect
 
             //understanding how function call happen in LINQ
             var someSequence = GenerateNumbers();
-            someSequence = someSequence.Select(n => n * n);
+            //someSequence = someSequence.Select(n => n * n);
 
             //equivalent to above statement
-            someSequence = someSequence
-                                        .Select((int n) =>
-                                            {
-                                                int b = 2;
-                                                return n * b;
-                                            });
+            //someSequence = someSequence
+            //                            .Select((int n) =>
+            //                                {
+            //                                    int b = 2;
+            //                                    return n * b;
+            //                                });
+            //for custom where
+            //sequence = sequence.Where(s => s.ToString().Length < 2);
+            //foreach (int val in someSequence)
+            //    Console.WriteLine(val);
 
-            sequence = sequence.Where(s => s.ToString().Length < 2);
-            foreach (int val in someSequence)
-                Console.WriteLine(val);
+            //For Custom Select
+            //var someselectSequence = sequence.Select(n => n.ToString());
 
+            //For Generic Custom Select
+            var someselectSequence = sequence.Where(n => n % 5 == 0).Select((n, index) =>
+                      new
+                      {
+                          index = ++index,
+                          formatedResult = n.ToString().PadLeft(20)
+                      });
+            foreach (var item in someselectSequence)
+                Console.WriteLine(item.index + "." + item.formatedResult);
             Console.ReadLine();
         }
 
@@ -49,6 +61,28 @@ namespace LINQSelect
             foreach (T item in source)
                 if (predicate(item))
                     yield return item;
+        }
+
+        public static IEnumerable<string> Select(this IEnumerable<int> source, Func<int, string> selector)
+        {
+            Console.WriteLine("Using Custom Implementation");
+            foreach (int item in source)
+                yield return selector(item);
+            Console.WriteLine("Using Custom Implementation");
+        }
+
+        public static IEnumerable<TResult> Select<TResult, TSource>(this IEnumerable<TSource> source, Func<TSource, TResult> selector)
+        {
+            Console.WriteLine("Using Generic Implementation Starts");
+            var save = Console.ForegroundColor;
+            foreach (TSource item in source)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("$item = " + item);
+                Console.ForegroundColor = save;
+                yield return selector(item);
+            }
+            Console.WriteLine("Using Generic Implementation Ends");
         }
     }
 }
